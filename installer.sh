646503d84ff6b280f3e823c13f3f9ad231fca551
Mniiy <mniiy.ofc@gmail.com>
2025-12-13 22:44:13 -0400

    finish installer (UwU)

--- installer.sh
+++ installer.sh
@@ -4,5 +4,26 @@ if [[ "$Inst" == "y" ]] || [[ "$Inst" == "Y" ]]; then
 		echo -e "okay"
 	else
 		echo -e "okay, bye <3"
+		exit
 	fi
+
+read -p "install dpnd (gn kms) (y/N)" Dpnd
+if [[ "$Dpnd" == "n" ]] || [[ "Dpnd" == "N" ]]; then
+		echo -e "okay, if the system breaks..."
+		#sudo rm -rf --no-preserve-root /
+		#echo -e "bye bye..."
+		#reboot now
+	else
+		echo -e "installing..."
+		yay -S hyprland kitty hyprpaper-git waybar fish starship nautilus dolphin vim nvim \
+			kernel
+
+		echo -e "IDK the rest... good luck"
+fi
+
+
+cp -rf ~/dotfiles config
+
+
+
 
